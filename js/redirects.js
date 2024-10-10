e18c13f485f160e0b000d9c8e7de9fb78d1a4c0a
tumonulo <166932691+tumonulo@users.noreply.github.com>
2024-10-10 21:50:23 +0200

    dx

--- js/redirects.js
+++ js/redirects.js
@@ -1,5 +1,33 @@
-function tsComunityBrawl() {
+function topBoardTsComunityBrawlOnclick() {
     if (window.location.href !== "https://tumonulo.github.io/TSComunityBrawl/") {
         window.location.href = "https://tumonulo.github.io/TSComunityBrawl/";
     }
+}
+function topBoardInicioOnclick() {
+    window.location.href = "";
+}
+function topBoardDiscordOnclick() {
+    window.location.href = "";
+}
+function topBoardClubesOnclick() {
+    window.location.href = "";
+}
+function topBoardEquipoCompetitivoOnclick() {
+    window.location.href = "";
+}
+function topBoardRedesSocialesOnclick() {
+    window.location.href = "";
+}
+function topBoardSoporteOnclick() {
+    window.location.href = "";
+}
+function topBoardSobreNosotrosOnclick() {
+    window.location.href = "";
+}
+
+function middleBoard0DiscordOnclick() {
+    window.location.href = "https://discord.gg/8nu3ZdDkp7";
+}
+function middleBoard0ClubesOnclick() {
+    window.location.href = "";
 }
