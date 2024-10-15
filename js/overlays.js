11bbaf282b4cc6be3746c89a46fbc89bb62024c0
tumonulo <juegosadrian777@gmail.com>
2024-10-15 20:11:58 +0200

    d

--- js/overlays.js
+++ js/overlays.js
@@ -1,5 +1,6 @@
-require('dotenv')
-const ID = ["#2C8QU2PLC", "#2UVYV998P", "#2UVYV998P", "#2R0L2CG9R", "#2G9U2P8L0", "#2G9U2P8L0", "#2VQLPVQJG"]
+require('dotenv');
+const tokenDiscordBot = process.env.TOKENDISCORDBOT;
+const ID = ["#2C8QU2PLC", "#2UVYV998P", "#2UVYV998P", "#2R0L2CG9R", "#2G9U2P8L0", "#2G9U2P8L0", "#2VQLPVQJG"];
 
 
 window.addEventListener("load", () => {
@@ -10,13 +11,24 @@ function middleBoardInicioOverlaySetValue() {
 const middleBoardInicioOverlay = document.getElementById("middleBoardInicioOverlay");
      const randomNumber = Math.floor(Math.random() * 4);
     if (randomNumber === 0) {
-        const middleBoardInicioOverlayValue0 = `Discord con x miembros`;
-        const middleBoardInicioOverlayValue0Format = /^Discord con \d+ miembros$/;
-        if (middleBoardInicioOverlayValue0Format.test(middleBoardInicioOverlay.innerText)) {
-            middleBoardInicioOverlaySetValue();
-        } else {
-            middleBoardInicioOverlay.innerText = middleBoardInicioOverlayValue0;
-        }
+        (async () => {
+            const requestDiscordMembers = fetch("", => {
+                headers {
+
+                } BODY: String.json
+            })
+
+
+
+
+            const middleBoardInicioOverlayValue0 = `Discord con x miembros`;
+            const middleBoardInicioOverlayValue0Format = /^Discord con \d+ miembros$/;
+            if (middleBoardInicioOverlayValue0Format.test(middleBoardInicioOverlay.innerText)) {
+                middleBoardInicioOverlaySetValue();
+            } else {
+                middleBoardInicioOverlay.innerText = middleBoardInicioOverlayValue0;
+            }
+        })()
     } else if (randomNumber === 1) {
         const middleBoardInicioOverlayValue1 = `Discord con x miembros activos`;
         const middleBoardInicioOverlayValue1Format = /^Discord con \d+ miembros activos$/;
