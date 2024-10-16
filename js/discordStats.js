222d2747b4c4cfc44fdf2a1f92effa7fc51ce76c
tumonulo <166932691+tumonulo@users.noreply.github.com>
2024-10-16 17:57:39 +0200

    n

---
+++ js/discordStats.js
@@ -0,0 +1,14 @@
+require('dotenv'); //REQUIRE ES SOLO DE NODE.JS ASI QUE TENGO Q MIRAR COMO HACER BACKEND CON NODE.JS!!!!!
+const tokenDiscordBot = process.env.TOKENDISCORDBOT;
+
+window.addEventListener("load", () => {
+    middleBoardDiscordStatsSetValue();
+});
+
+async function middleBoardDiscordStatsSetValue() {
+    Promise.all([
+
+    ]).then(() => {
+        console.log("bhnkml,");
+    })
+}
